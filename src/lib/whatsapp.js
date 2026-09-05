c161d57703582f2e6638ac5903c372edf25fba07
Caleb Ariko <calebouma202@gmail.com>
2026-09-05 23:10:39 +0300

    deleted a photo which was not being used and updated the whatsapp.js file

--- src/lib/whatsapp.js
+++ src/lib/whatsapp.js
@@ -1,4 +1,4 @@
-const WHATSAPP_NUMBER = "254702011192"; // TODO: replace with client number
+const WHATSAPP_NUMBER = "254702011192";
 
 export function whatsappLink(message) {
   return `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`;
